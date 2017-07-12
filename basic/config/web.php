866120433c49c1fee7659a49c889234bdbd8bc43
CodingPete <peter.meyer@stud.fh-flensburg.de>
2017-07-12 14:50:42 +0200

    first commits

--- basic/config/web.php
+++ basic/config/web.php
@@ -63,9 +63,8 @@ if (YII_ENV_DEV) {
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
-        'allowedIPs' => ['XXX.XXX.XXX.XXX', 'YYY.YYY.YYY.YYY']
         // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
+        'allowedIPs' => ['127.0.0.1', '::1'],
     ];
 }
 
