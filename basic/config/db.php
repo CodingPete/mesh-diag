4ea4f9afd6bceca3cfca51d2d19dad79e5d0cf85
CodingPete <peter.meyer@stud.fh-flensburg.de>
2017-07-12 12:00:44 +0200

    first commits

--- basic/config/db.php
+++ basic/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=mesh',
+    'username' => 'mesh-diag',
+    'password' => 'Fn1qe*33',
     'charset' => 'utf8',
 ];
